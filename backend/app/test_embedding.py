82aad5e0c200909849f48438046b9c4bf130bbd1
Amreen <amreenbegum75789@gmail.com>
2026-05-26 14:34:40 +0530

    Added Dockerfile for GCP deployment

--- backend/app/test_embedding.py
+++
@@ -1,9 +0,0 @@
-from embeddings import generate_embedding
-
-text = "Attention mechanisms in transformers"
-
-embedding = generate_embedding(text)
-
-print(type(embedding))
-print(len(embedding))
-print(embedding[:10])
